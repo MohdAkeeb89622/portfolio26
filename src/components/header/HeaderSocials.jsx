d916c9f50bf2983f99e6f1025ecd777c64105e6f
Akeeb Khan <akeeb260996@gmail.com>
2026-01-14 14:08:56 +0530

    Fix Render deployment: Add rel='noreferrer' to all external links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {ImLinkedin} from 'react-icons/im'
-import {ImGithub} from 'react-icons/im'
-import {ImInstagram} from 'react-icons/im'
+import { ImLinkedin } from 'react-icons/im'
+import { ImGithub } from 'react-icons/im'
+import { ImInstagram } from 'react-icons/im'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/mohd-akeeb-khan-766885176/" target="_blank"><ImLinkedin/></a>
-        <a href="https://github.com/MohdAkeeb89622" target="_blank"><ImGithub/></a>
-        <a href="https://instagram.com/md__akeeb__khan?igshid=YmMyMTA2M2Y=" target="_blank"><ImInstagram/></a>
+      <a href="https://www.linkedin.com/in/mohd-akeeb-khan-766885176/" target="_blank" rel="noreferrer"><ImLinkedin /></a>
+      <a href="https://github.com/MohdAkeeb89622" target="_blank" rel="noreferrer"><ImGithub /></a>
+      <a href="https://instagram.com/md__akeeb__khan?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer"><ImInstagram /></a>
     </div>
   )
 }
